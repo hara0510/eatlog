63b8c8b18633da42781ad878fd75905842bc879c
hara0510 <oqj0510@gmail.com>
2023-11-08 19:46:30 +0900

    Active Storageの導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,12 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:memo, :meal_time, :image)
+    # .merge(user_id: current_user.id)
+  end
 end
