6ffa1332a19a72d87bd570f3388c944b97397e2d
hara0510 <oqj0510@gmail.com>
2023-11-15 23:30:45 +0900

    詳細表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  resources :posts, only: [:index, :new, :create] do
+  resources :posts, only: [:index, :new, :create, :show] do
     collection do
       get 'show_by_mealtime/:mealtime_id', to: 'posts#show_by_mealtime', as: 'show_by_mealtime'
     end
