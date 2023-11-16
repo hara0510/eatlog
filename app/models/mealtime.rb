d4491b33f481fb5a8a0c9eb7b9aae6084e248202
hara0510 <oqj0510@gmail.com>
2023-11-16 23:26:44 +0900

    新規投稿、編集viewの修正

--- app/models/mealtime.rb
+++ app/models/mealtime.rb
@@ -1,6 +1,6 @@
 class Mealtime < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' },
+    { id: 1, name: '食べた時間を選択' },
     { id: 2, name: 'あさ' },
     { id: 3, name: 'ひる' },
     { id: 4, name: 'よる' },
