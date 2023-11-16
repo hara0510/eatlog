45c517a881da182f2ca1656408d957cd9ef4bde0
hara0510 <oqj0510@gmail.com>
2023-11-16 22:02:01 +0900

    destroyアクションの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action:set_post, only: [:edit, :show, :update]
+  before_action:set_post, only: [:edit, :show, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -39,6 +39,12 @@ class PostsController < ApplicationController
       render:edit, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+
 
   private
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
-  resources :posts, only: [:index, :new, :create, :show, :edit, :update] do
+  resources :posts do
     collection do
       get 'show_by_mealtime/:mealtime_id', to: 'posts#show_by_mealtime', as: 'show_by_mealtime'
     end
