a393838668fea08a5a0c8ca47c465f70ddc55052
hara0510 <oqj0510@gmail.com>
2023-11-17 23:50:59 +0900

    テストコードの実装

--- Gemfile
+++ Gemfile
@@ -78,3 +78,6 @@ end
 gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
 gem 'active_hash'
+gem 'rspec-rails', '~> 4.0.0'
+gem 'factory_bot_rails'
+gem 'faker'
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  before do
+    @post = FactoryBot.build(:post)
+  end
+
+  describe '新規投稿' do
+    context '投稿できる場合' do
+      it 'imageとmealtime_idがあれば登録できる' do
+        expect(@post).to be_valid
+      end
+
+      it 'memoがなくても登録できる' do
+        @post.memo = ''
+        expect(@post).to be_valid
+      end
+    end
+
+    context '投稿できない場合' do
+      it 'imageが空では投稿できない' do
+        @post.image = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include "Image can't be blank"
+      end
+
+      it 'mealtime_idが空では投稿できない' do
+        @post.mealtime_id = '1'
+        @post.valid?
+        expect(@post.errors.full_messages).to include "Mealtime must be other than 1"
+      end
+
+      # it 'userが紐づいていないと登録できない' 
+    end
+  end
+end
