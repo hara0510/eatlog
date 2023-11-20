9a3b9fa7c65d869110e7f7ad99c4b088cac9c34e
hara0510 <oqj0510@gmail.com>
2023-11-20 21:46:56 +0900

    deviseの導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_07_120650) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_20_123024) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -47,9 +47,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_07_120650) do
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
-    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", null: false
     t.text "bio"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
