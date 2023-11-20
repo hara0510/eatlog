e80da0fe68bbe17e242846bdade7cb25a8fad361
hara0510 <oqj0510@gmail.com>
2023-11-21 00:06:29 +0900

    アソシエーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :mealtime
   has_one_attached :image
+  has_many :posts
 
   validates :image, presence: true
   validates :mealtime_id, numericality: { other_than: 1 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  belongs_to :user
+
   validates :nickname, presence: true
 end
