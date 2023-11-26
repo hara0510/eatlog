affc9d580952ff3e65886f52419e4b220d03b815
hara0510 <oqj0510@gmail.com>
2023-11-26 20:56:28 +0900

    favoriteテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_22_130759) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_26_115310) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,6 +43,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_22_130759) do
     t.integer "user_id"
     t.integer "post_id"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", charset: "utf8", force: :cascade do |t|
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
