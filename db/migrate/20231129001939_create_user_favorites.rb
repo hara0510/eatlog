da67a940f45c4940a7d452b8ebc79fc3ef4cc4b7
hara0510 <oqj0510@gmail.com>
2023-11-29 09:25:45 +0900

    user_favoritesテーブルの作成

---
+++ db/migrate/20231129001939_create_user_favorites.rb
@@ -0,0 +1,9 @@
+class CreateUserFavorites < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_favorites do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :favorite, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_26_115310) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_29_001939) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -61,6 +61,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_26_115310) do
     t.integer "user_id"
   end
 
+  create_table "user_favorites", charset: "utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "favorite_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["favorite_id"], name: "index_user_favorites_on_favorite_id"
+    t.index ["user_id"], name: "index_user_favorites_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -77,4 +86,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_26_115310) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "user_favorites", "favorites"
+  add_foreign_key "user_favorites", "users"
 end
