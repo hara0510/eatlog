2a586d8a97edcd872647a24249fe49e748a020af
hara0510 <oqj0510@gmail.com>
2023-11-30 11:11:52 +0900

    コントローラーの設定

---
+++ app/controllers/follows_controller.rb
@@ -0,0 +1,13 @@
+class FollowsController < ApplicationController
+  def create
+    follow = current_user.active_follows.new(follower_id: params[:user_id])
+    follow.save
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    follow = current_user.active_follows.find_by(follower_id: params[:user_id])
+    follow.destroy
+    redirect_back(fallback_location: root_path)
+  end
+end
