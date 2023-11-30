184f00c4b67d2944fa68fb87a519a57d97f75c8f
hara0510 <oqj0510@gmail.com>
2023-11-30 10:24:26 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,11 @@ class User < ApplicationRecord
   has_many :user_favorites
   has_many :favorites, through: :user_favorites
 
+  has_many :active_follows, class_name: "Follow", foreign_key: :following_id
+  has_many :followings, through: :active_follows, source: :follower
+
+  has_many :passive_follows, class_name:"Follow", foreign_key: :follower_id
+  has_many :followers, through: :active_follows, source: :following
+
   validates :nickname, presence: true
 end
